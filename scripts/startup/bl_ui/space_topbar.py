7134e73299c8b54137eb6a1ef69fdda6db2c1a46
Jonas Holzman <jonas@holzman.fr>
2025-08-21 14:33:19 +0200

    iOS: Improve external file support
    Improve handling of external files, implements the iOS start/stop
    security scope resource access paradigms used for opening files, as
    well as iOS bundle property modifications to allow Blender to have it's
    own iOS Document folder (accessible from On My iPad > Blender in the
    Files app) and properly recognize file type.
    
    The Blender Document folder is also now accessible in the File Browser
    from the System shelf, removing the now outdated "Bundle Assets" folder.
    
    What works:
    - Opening / Saving files in Blender's own Document folder
    - Opening files from anywhere on the iPad (use the share to Blender
      action in the Files Browser)
    
    What doesn't work:
    - Saving files opened from outside Blender's own Document folder in
      place. To workaround this limitation, the Save operator is disabled
      for files, requiring the user to save the file
    - Opening files outside Blender's own Document folder from the Blender
      File Browser, the iOS Files app has to be used, in the near future,
      the iOS File Browser could be integrated directly in Blender to
      workaround such sandbox limitations.

--- scripts/startup/bl_ui/space_topbar.py
+++ scripts/startup/bl_ui/space_topbar.py
@@ -169,8 +169,17 @@ class TOPBAR_MT_file(Menu):
 
         layout.separator()
 
-        layout.operator_context = 'EXEC_AREA' if context.blend_data.is_saved else 'INVOKE_AREA'
-        layout.operator("wm.save_mainfile", text="Save", icon='FILE_TICK').show_save_modified_images_dialog = True
+        # iOS Workaround: Provide a clean alternative to write errors caused by sandbox restrictions
+        import os
+        file_is_writable = os.access(bpy.data.filepath, os.W_OK)
+
+        sub = layout.row()
+        sub.enabled = file_is_writable
+        sub.operator_context = 'EXEC_AREA' if context.blend_data.is_saved else 'INVOKE_AREA'
+        sub.operator("wm.save_mainfile", text="Save", icon='FILE_TICK').show_save_modified_images_dialog = True
+
+        layout.operator_context = 'INVOKE_AREA'
+        layout.operator("wm.save_as_mainfile", text="Save", icon='FILE_TICK').show_save_modified_images_dialog = True
 
         layout.operator_context = 'INVOKE_AREA'
         layout.operator("wm.save_as_mainfile", text="Save As...").show_save_modified_images_dialog = True
@@ -180,7 +189,7 @@ class TOPBAR_MT_file(Menu):
         save_copy.show_save_modified_images_dialog = True
 
         sub = layout.row()
-        sub.enabled = context.blend_data.is_saved
+        sub.enabled = context.blend_data.is_saved and file_is_writable
         sub.operator_context = 'EXEC_AREA'
         save_incremental = sub.operator("wm.save_mainfile", text="Save Incremental")
         save_incremental.incremental = True
